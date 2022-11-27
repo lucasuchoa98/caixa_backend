c0edab5bed031bb4ad41f37ed38414e1d06e5642
lucasuchoa98 <lucasuchoalg@gmail.com>
2022-11-27 19:28:26 -0300

    backend

--- caixa_backend/api_constuctor/api/viewsets.py
+++ caixa_backend/api_constuctor/api/viewsets.py
@@ -2,10 +2,14 @@ from rest_framework import viewsets
 from api_constuctor.api import serializers
 from api_constuctor import models
 
+from rest_framework.permissions import AllowAny
+
 class ItemViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ItemSerializers
     queryset = models.Item.objects.all()
+    permission_classes = [AllowAny]
 
 class VendaViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.VendaSerializers
     queryset = models.Item.objects.all()
+    permission_classes = [AllowAny]
